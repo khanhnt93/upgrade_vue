5e1f9dc62485bc6d99b96d8d35c45151aece6986
PhuHV.56 <PhuHV.56@gmail.com>
2026-04-14 00:02:45 +0700

    Update UI bug

--- other_projects/ciPro/src/views/admin/repository/RepositoryInput.vue
+++ other_projects/ciPro/src/views/admin/repository/RepositoryInput.vue
@@ -35,7 +35,7 @@
                 class="flex-1"
               />
               <button
-                @click="showModalSearchOrderBuy"
+                @click="showModalSearchOrderBuy = true"
                 title="Tìm kiếm"
                 class="px-4 py-2 bg-white text-blue-600 border border-blue-600 rounded hover:bg-blue-50 transition-colors"
               >
